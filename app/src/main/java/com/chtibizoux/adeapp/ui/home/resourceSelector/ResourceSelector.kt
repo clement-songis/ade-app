445f74a047b504a07b2beda32bebc7fd45716cdf
Clément <5clemans585@gmail.com>
2024-01-19 14:17:29 +0100

    remove unnecessary lines

--- app/src/main/java/com/chtibizoux/adeapp/ui/home/resourceSelector/ResourceSelector.kt
+++ app/src/main/java/com/chtibizoux/adeapp/ui/home/resourceSelector/ResourceSelector.kt
@@ -3,18 +3,14 @@ package com.chtibizoux.adeapp.ui.home.resourceSelector
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -29,7 +25,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -42,11 +37,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.chtibizoux.adeapp.R
@@ -124,11 +117,8 @@ fun ResourceSelectorContent(navController: NavController, resources: ResourceTre
 
         Column(
             modifier = Modifier
-//                .padding(20.dp)
                 .fillMaxWidth()
-                .verticalScroll(rememberScrollState()),
-//            verticalArrangement = Arrangement.spacedBy(20.dp),
-//            horizontalAlignment = Alignment.CenterHorizontally
+                .verticalScroll(rememberScrollState())
         ) {
             val category = resources.categories[tabIndex]
             category.resources.sortedBy { it.name }.forEach { resource ->
@@ -176,7 +166,6 @@ fun ResourceComponent(navController: NavController, category: String, resource: 
                     Icons.Filled.KeyboardArrowDown,
                     stringResource(R.string.more),
                     modifier = Modifier
-//                        .padding(start = 20.dp, top = 20.dp, bottom = 20.dp)
                         .background(
                             MaterialTheme.colorScheme.secondaryContainer, RoundedCornerShape(50)
                         )
@@ -184,7 +173,6 @@ fun ResourceComponent(navController: NavController, category: String, resource: 
                 )
             }
 
-            /*TODO*/
             if (opened) {
                 Column(Modifier.padding(start = 20.dp)) {
                     resource.children.sortedBy { it.name }.forEach {
