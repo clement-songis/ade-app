f82ecb3a3a0e2a4bcf00e03918fec7fd8ad5dfb1
Clément <5clemans585@gmail.com>
2024-06-24 23:01:49 +0200

    Remove unused imports

--- app/src/main/java/com/chtibizoux/adeapp/alarms/AlarmActivity.kt
+++ app/src/main/java/com/chtibizoux/adeapp/alarms/AlarmActivity.kt
@@ -16,10 +16,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,7 +25,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.chtibizoux.adeapp.R
 import com.chtibizoux.adeapp.ui.theme.ADEAppTheme
