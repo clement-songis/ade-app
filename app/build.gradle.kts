240d9fbe0a353598ab44f51f33cf1cbffd65af72
Clément Songis <5clemans585@gmail.com>
2025-02-11 20:22:50 +0100

    Upgrade android target version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.chtibizoux.adeapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.chtibizoux.adeapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
 
         versionName = getLatestGitTag()
         versionCode = getVersionCode(versionName!!)
@@ -88,21 +88,21 @@ fun getVersionCode(tag: String): Int {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation(platform("androidx.compose:compose-bom:2025.01.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.3.1")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
-    implementation("androidx.compose.material:material-icons-extended:1.7.6")
-    implementation("androidx.datastore:datastore:1.1.1")
+    implementation("androidx.navigation:navigation-compose:2.8.6")
+    implementation("androidx.compose.material:material-icons-extended:1.7.7")
+    implementation("androidx.datastore:datastore:1.1.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.01.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.6")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.7")
 }
